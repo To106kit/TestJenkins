22aa0aeb457def6c815038936715ca0c31aeeb46
To106kit <t.to106ki@gmail.com>
2022-06-19 17:12:06 +0900

    test

--- MBD_SwUnit/CodeGen.groovy
+++ MBD_SwUnit/CodeGen.groovy
@@ -18,9 +18,9 @@
  */
 
 // Read Jenkins Shared Library
-@Library('SPIFWLibrary@R2021.B.04')
+// @Library('SPIFWLibrary@R2021.B.04')
 
-import cictframework._common.CICTFrameworkException
+// import cictframework._common.CICTFrameworkException
 
 def yamlConfig
 def processName = 'mdl_coding'
@@ -50,129 +50,129 @@ pipeline {
                 }
             }
         }
-        stage('Execute') {
-            agent {
-                node {
-                    label yamlConfig.labelName
-                    customWorkspace custom_workspace
-                }
-            }
-            steps {
-                script {
-                    def searchResults
-                    yamlConfig = configuration.read("${params.netDriveUsespace}\\Config\\${processName}\\${toolName}\\${toolName}.yaml")   // to replace env ver of Jenkins
+        // stage('Execute') {
+        //     agent {
+        //         node {
+        //             label yamlConfig.labelName
+        //             customWorkspace custom_workspace
+        //         }
+        //     }
+        //     steps {
+        //         script {
+        //             def searchResults
+        //             yamlConfig = configuration.read("${params.netDriveUsespace}\\Config\\${processName}\\${toolName}\\${toolName}.yaml")   // to replace env ver of Jenkins
 
-                    try {
-                        // Prepare workspace
-                        prepareWorkspace(yamlConfig, processName, toolName, params.netDriveUsespace)
+        //             try {
+        //                 // Prepare workspace
+        //                 prepareWorkspace(yamlConfig, processName, toolName, params.netDriveUsespace)
 
-                        // Replace place holder "<UNIT_NAME>" with Jenkins job folder name
-                        def jFolderName = env.JOB_NAME.split('/')
-                        jFolderName = jFolderName[jFolderName.size()-3]
+        //                 // Replace place holder "<UNIT_NAME>" with Jenkins job folder name
+        //                 def jFolderName = env.JOB_NAME.split('/')
+        //                 jFolderName = jFolderName[jFolderName.size()-3]
                         
-                        Map unitPlaceHolder = [:]
-                        unitPlaceHolder['<UNIT_NAME>'] = jFolderName
-                        yamlConfig = searchfiles.replacePlaceHolders(yamlConfig, unitPlaceHolder)
+        //                 Map unitPlaceHolder = [:]
+        //                 unitPlaceHolder['<UNIT_NAME>'] = jFolderName
+        //                 yamlConfig = searchfiles.replacePlaceHolders(yamlConfig, unitPlaceHolder)
 
-                        // Search model files
-                        searchResults = mbdCommonProcessing.searchModelFiles(yamlConfig)
+        //                 // Search model files
+        //                 searchResults = mbdCommonProcessing.searchModelFiles(yamlConfig)
 
-                        // Display list of models
-                        def searchMsgs = []
-                        searchMsgs << '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
-                        searchMsgs << '[CI/CT] [Information] The list of models to be executed is as follows:'
-                        searchResults.fileName.each { ele ->
-                            searchMsgs << ele
-                        }
-                        searchMsgs << '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
-                        println searchMsgs.join('\n')
-                    } catch (CICTFrameworkException e) {
-                        printMessage(e)
-                        currentBuild.result = 'FAILURE'
-                        throw e
-                    }
+        //                 // Display list of models
+        //                 def searchMsgs = []
+        //                 searchMsgs << '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+        //                 searchMsgs << '[CI/CT] [Information] The list of models to be executed is as follows:'
+        //                 searchResults.fileName.each { ele ->
+        //                     searchMsgs << ele
+        //                 }
+        //                 searchMsgs << '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+        //                 println searchMsgs.join('\n')
+        //             } catch (CICTFrameworkException e) {
+        //                 printMessage(e)
+        //                 currentBuild.result = 'FAILURE'
+        //                 throw e
+        //             }
 
-                    // Process to execute tool
-                    try {
-                        def errorModels = []
-                        for (searchResult in searchResults) {
-                            Map modelPlaceHolders = [:]
-                            modelPlaceHolders['<MODEL_FILE>'] = searchResult.fileName
-                            modelPlaceHolders['<MODEL_DIR>'] = searchResult.fileDir
-                            modelPlaceHolders['<MODEL_BASE_NAME>'] = searchResult.basename
+        //             // Process to execute tool
+        //             try {
+        //                 def errorModels = []
+        //                 for (searchResult in searchResults) {
+        //                     Map modelPlaceHolders = [:]
+        //                     modelPlaceHolders['<MODEL_FILE>'] = searchResult.fileName
+        //                     modelPlaceHolders['<MODEL_DIR>'] = searchResult.fileDir
+        //                     modelPlaceHolders['<MODEL_BASE_NAME>'] = searchResult.basename
 
-                            def newYamlConfig = searchfiles.replacePlaceHolders(yamlConfig, modelPlaceHolders)
-                            try {
-                                // Execute CodeGen
-                                codeGen(newYamlConfig.parameters)
-                            } catch (CICTFrameworkException e) {
-                                if (e.id.equals('0005600003')) {   // MATLAB returned nonzero exit
-                                    Map errinfo = e.errinfo
-                                    def quotientRetVal = (int)(errinfo.matlabRetVal / 100)
+        //                     def newYamlConfig = searchfiles.replacePlaceHolders(yamlConfig, modelPlaceHolders)
+        //                     try {
+        //                         // Execute CodeGen
+        //                         codeGen(newYamlConfig.parameters)
+        //                     } catch (CICTFrameworkException e) {
+        //                         if (e.id.equals('0005600003')) {   // MATLAB returned nonzero exit
+        //                             Map errinfo = e.errinfo
+        //                             def quotientRetVal = (int)(errinfo.matlabRetVal / 100)
 
-                                    if (quotientRetVal == 1) {   // Warning => UNSTABLE
-                                        printMessage('1005600001', errinfo.matlabRetVal.toString())
-                                        currentBuild.result = 'UNSTABLE'
-                                    } else if (quotientRetVal == 2) {   // Error => FAILURE
-                                        printMessage(e.id, errinfo.matlabRetVal.toString())
-                                        printMessage('0008600001', searchResult.fileName)
-                                        errorModels << searchResult.fileName
-                                    } else {
-                                        printMessage(e.id, errinfo.matlabRetVal.toString())
-                                        printMessage('0005600004')
-                                        printMessage('0008600001', searchResult.fileName)
-                                        errorModels << searchResult.fileName
-                                    }
-                                } else {   // other error (e.g. validate parameters)
-                                    printMessage(e)
-                                    printMessage('0008600001', searchResult.fileName)
-                                    errorModels << searchResult.fileName
-                                }
-                            } catch (Exception e) {
-                                println e.message
-                                printMessage('0008600001', searchResult.fileName)
-                                errorModels << searchResult.fileName
-                            } finally {
-                                // Save results
-                                fileOperations([
-                                    folderCreateOperation(folderPath:"Result\\${searchResult.basename}")
-                                ])
+        //                             if (quotientRetVal == 1) {   // Warning => UNSTABLE
+        //                                 printMessage('1005600001', errinfo.matlabRetVal.toString())
+        //                                 currentBuild.result = 'UNSTABLE'
+        //                             } else if (quotientRetVal == 2) {   // Error => FAILURE
+        //                                 printMessage(e.id, errinfo.matlabRetVal.toString())
+        //                                 printMessage('0008600001', searchResult.fileName)
+        //                                 errorModels << searchResult.fileName
+        //                             } else {
+        //                                 printMessage(e.id, errinfo.matlabRetVal.toString())
+        //                                 printMessage('0005600004')
+        //                                 printMessage('0008600001', searchResult.fileName)
+        //                                 errorModels << searchResult.fileName
+        //                             }
+        //                         } else {   // other error (e.g. validate parameters)
+        //                             printMessage(e)
+        //                             printMessage('0008600001', searchResult.fileName)
+        //                             errorModels << searchResult.fileName
+        //                         }
+        //                     } catch (Exception e) {
+        //                         println e.message
+        //                         printMessage('0008600001', searchResult.fileName)
+        //                         errorModels << searchResult.fileName
+        //                     } finally {
+        //                         // Save results
+        //                         fileOperations([
+        //                             folderCreateOperation(folderPath:"Result\\${searchResult.basename}")
+        //                         ])
                                 
-                                String t_post = 'code'
-                                if (fileExists("${searchResult.fileDir}\\${searchResult.basename}_${t_post}")) {
-                                    fileOperations([
-                                        folderRenameOperation(source:"${searchResult.fileDir}\\${searchResult.basename}_${t_post}", destination:"Result\\${searchResult.basename}\\${searchResult.basename}_${t_post}")
-                                    ])
-                                }
-                            }
-                        }
+        //                         String t_post = 'code'
+        //                         if (fileExists("${searchResult.fileDir}\\${searchResult.basename}_${t_post}")) {
+        //                             fileOperations([
+        //                                 folderRenameOperation(source:"${searchResult.fileDir}\\${searchResult.basename}_${t_post}", destination:"Result\\${searchResult.basename}\\${searchResult.basename}_${t_post}")
+        //                             ])
+        //                         }
+        //                     }
+        //                 }
 
-                        // Display execution results
-                        def resultMsgs = []
-                        resultMsgs << '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
-                        resultMsgs << '[CI/CT] [Information] The results of execution for each models are as follows:'
-                        searchResults.fileName.each { ele ->
-                            def resultMsg = errorModels.contains(ele) ? 'SKIP: ' + ele : '    : ' + ele
-                            resultMsgs << resultMsg
-                        }
-                        resultMsgs << '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
-                        println resultMsgs.join('\n')
+        //                 // Display execution results
+        //                 def resultMsgs = []
+        //                 resultMsgs << '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+        //                 resultMsgs << '[CI/CT] [Information] The results of execution for each models are as follows:'
+        //                 searchResults.fileName.each { ele ->
+        //                     def resultMsg = errorModels.contains(ele) ? 'SKIP: ' + ele : '    : ' + ele
+        //                     resultMsgs << resultMsg
+        //                 }
+        //                 resultMsgs << '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+        //                 println resultMsgs.join('\n')
 
-                        if (!errorModels.isEmpty()) {
-                            currentBuild.result = searchResults.fileName == errorModels ? 'FAILURE' : 'UNSTABLE'
-                        }
-                    } catch (Exception e) {
-                        currentBuild.result = 'FAILURE'
-                        throw e
-                    } finally {
-                        // Post-processing
-                        mbdCommonProcessing.deletePreJobResultFoldersOnWorkspace(yamlConfig)
-                        mbdCommonProcessing.uploadResultsByArchiveArtifacts(yamlConfig, currentBuild.currentResult)
-                        mbdCommonProcessing.uploadResultsToNetDrive(yamlConfig, currentBuild.currentResult, processName, toolName, params.netDriveUsespace)
-                    }
-                }
-            }
-        }
+        //                 if (!errorModels.isEmpty()) {
+        //                     currentBuild.result = searchResults.fileName == errorModels ? 'FAILURE' : 'UNSTABLE'
+        //                 }
+        //             } catch (Exception e) {
+        //                 currentBuild.result = 'FAILURE'
+        //                 throw e
+        //             } finally {
+        //                 // Post-processing
+        //                 mbdCommonProcessing.deletePreJobResultFoldersOnWorkspace(yamlConfig)
+        //                 mbdCommonProcessing.uploadResultsByArchiveArtifacts(yamlConfig, currentBuild.currentResult)
+        //                 mbdCommonProcessing.uploadResultsToNetDrive(yamlConfig, currentBuild.currentResult, processName, toolName, params.netDriveUsespace)
+        //             }
+        //         }
+        //     }
+        // }
     }
 }
 
@@ -180,10 +180,10 @@ pipeline {
  * The following functions are private functions.
  ********************************************************************************/
 
-void prepareWorkspace(Map config, String process, String tool, String containerPath) {
-    mbdCommonProcessing.initializeWorkspace()
-    mbdCommonProcessing.downloadConfigFolder(process, tool, containerPath)
-    mbdCommonProcessing.downloadSourceFolders(config, tool, containerPath)
-    // mbdCommonProcessing.downloadConfigurationFileFromServer(config, tool)
-    mbdCommonProcessing.downloadPreJobResult(config, tool, containerPath)
-}
+// void prepareWorkspace(Map config, String process, String tool, String containerPath) {
+//     mbdCommonProcessing.initializeWorkspace()
+//     mbdCommonProcessing.downloadConfigFolder(process, tool, containerPath)
+//     mbdCommonProcessing.downloadSourceFolders(config, tool, containerPath)
+//     // mbdCommonProcessing.downloadConfigurationFileFromServer(config, tool)
+//     mbdCommonProcessing.downloadPreJobResult(config, tool, containerPath)
+// }
